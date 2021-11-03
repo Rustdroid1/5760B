431c531bdf31d1eac52601026963c1f6f96101fa
Rustdroid1 <62392212+Rustdroid1@users.noreply.github.com>
2021-11-03 15:43:17 +1030

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_ac8257_demo_1g_32.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_ac8257_demo_1g_32-user \
+    omni_ac8257_demo_1g_32-userdebug \
+    omni_ac8257_demo_1g_32-eng
